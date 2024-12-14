48166b404b491d83fde86bd472e7681e8cdba9ea
Richie <722461@student.dusd.net>
2024-12-13 23:59:58 -0800

    Fixed the scripting

--- FlappyFinalMarquezP2/Assets/Scripts/GameControl.cs
+++ FlappyFinalMarquezP2/Assets/Scripts/GameControl.cs
@@ -41,7 +41,7 @@ public class GameControl : MonoBehaviour  {
             return;
         }
         score++;
-        scoreText.text = "Score: " + score.ToString();
+        scoreText.text = "Score: " + score.ToString ();
     }
     public void BirdDied()
     {
